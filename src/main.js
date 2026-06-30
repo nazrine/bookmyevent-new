a67fbf4365e77f79cb534870d54ac37ecba4a614
nazrine <fathimanazrinem@gmail.com>
2026-06-30 14:04:56 +0530

    new project bookmyevent-new-router and bootstrap installed

---
+++ src/main.js
@@ -0,0 +1,9 @@
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+createApp(App).use(router).mount('#app')
